1612e86d21d6e4b20f316cc8b2574be4a1063952
Richard Beamish <richardb@techfabric.co>
2019-09-06 11:30:49 -0700

    added a DTO for the product class

---
+++ Domain/DTO/ProductDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Domain.DTO
+{
+    public class ProductDTO
+    {
+        public string SKU { get; set; }
+        public int Vintage { get; set; }
+        public string ItemName { get; set; }
+        public int ListPrice { get; set; }
+        public string ImageSmallURL { get; set; }
+        public string SpecificAppellation { get; set; }
+        public string Varietal { get; set; }
+        public int OutOfStockYN { get; set; }
+    }
+}
