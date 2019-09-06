609fe3a62276c0478bb9c500bd81d7e94cfa184e
Richard Beamish <richardb@techfabric.co>
2019-09-06 11:43:16 -0700

    chore: added method to update products directly from their own grains

--- Grains/RecommendationGrain.cs
+++ Grains/RecommendationGrain.cs
@@ -1,23 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Domain;
-using Domain.Core.Exceptions;
+using Domain.DTO;
 using GrainInterfaces;
 using Orleans;
 
 namespace Grains
 {
+
+    // this.State should store a list of all recommended products for the given product
+
     public class RecommendationGrain : Grain<Recommendation>, IRecommendationGrain
     {
-        public int RecommendationsGrainId;
+        // a list of products with only the necessary properties to display them in the recommendations que
+        public List<ProductDTO> Recommendations;
 
-        public async Task<List<SKU>> GetRecommendedSKUs(string productId)
+
+        public async Task UpdateRecommendations()
         {
-            var recommendationsGrain = this.GrainFactory.GetGrain<IRecommendationsGrain>(RecommendationsGrainId);
-            var recommendation = await recommendationsGrain.GetProductRecommendation(productId);
-            return recommendation.RecommendedSKUs;
+            var recommendations = new List<ProductDTO>();
+
+            this.State.RecommendedProducts.ForEach(product => recommendations.Add(
+                new ProductDTO()
+                {
+                    SKU = product.Record.Sku,
+                    Vintage = product.Record.Vintage,
+                    ItemName = product.Record.ItemName,
+                    ListPrice = product.Record.ListPrice,
+                    ImageSmallURL = product.Record.ImageSmallURL,
+                    SpecificAppellation = product.Record.SpecificAppellation,
+                    Varietal = product.Record.Varietal,
+                    OutOfStockYN = product.Record.OutOfStockYN
+                })
+            );
+
+            Recommendations = recommendations;
+
+            await CheckAvailability();
+
+            if (Recommendations.Count < 5)
+            {
+                await AddMoreRecommendations();
+            }
+        }
+
+
+        public async Task CheckAvailability()
+        {
+            Recommendations.ForEach(recommendation =>
+            {
+                if (recommendation.OutOfStockYN == 1)
+                {
+                    Recommendations.Remove(recommendation);
+                }
+            });
+        }
+
+        public async Task AddMoreRecommendations()
+        {
+            // Adds recommendations using a different algorythm..?
+        }
+
+
+        public async Task<List<ProductDTO>> GetRecommendations()
+        {
+            return Recommendations;
         }
     }
 }
